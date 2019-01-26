058ebde1e20f7808aad7ead3199fc0b9ff8d9c29
Hiroshi Tone <valkyrie.gtr@gmail.com>
2019-01-26 13:08:33 +0900

    教科書どおりに入力完了。

--- AItestApp/ViewController.swift
+++ AItestApp/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate,UIImagePickerControllerDelegate {
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var myTextView: UITextView!
+    
+    // カメラロールを表示するimagePicker
+    var imagePicker: UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // imagePickerと、テキストビューを初期化
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        myTextView.text = ""
     }
-
-
+    
+    @IBAction func tapButton(_ sender: Any) {
+        // カメラロールを表示する
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    // カメラロールで処理が終わった時に呼び出される
+    func imagePickerController(_ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [String : Any]) {
+        // カメラロールを閉じて
+        imagePicker.dismiss(animated: true, completion: nil)
+        // 選択した画像が存在すれば
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            return
+        }
+        // イメージビューの表示する
+        myImageView.image = image
+    }
 }
-
